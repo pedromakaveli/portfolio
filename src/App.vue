e1285a897ae361491646cd656b679160ba09ce88
pedromakaveli <pedrolacerda804@gmail.com>
2023-12-30 20:18:19 -0300

    Add portfolio section

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,21 @@
     </div>
     <!-- Finaliza a div de apresentação  -->
 
+    <!-- Inicia a div do portfolio -->
+    <div class="divide-y divide-solid">
+      <div class="container min-h-[100vh] mx-auto p-20 flex flex-col">
+        <h1 class="cursor-pointer text-center text-sky-100 text-[1.5rem] hover:text-cyan-400"><span
+            class="text-cyan-400 text-[2.2rem]">{</span> Meus projetos <span class="text-cyan-400 text-[2.2rem]">}</span>
+        </h1>
+
+        <div class="mt-[100px] grid grid-cols-2 text-sky-100">
+
+        </div>
+
+      </div>
+    </div>
+    <!-- Finaliza a div do portfolio -->
+
   </div>
 </template>
 

2c498c260aa45a311423ebfd92fe6f86caebebcc
pedromakaveli <pedrolacerda804@gmail.com>
2023-12-31 03:20:54 -0300

    qqPush the arrow icon down

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
           <a class="bg-pink text-white pt-4 pb-4 px-10 uppercase font-roboto-mono" href="#">Linkedin</a>
         </div>
 
-        <div class="cursor-pointer animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
+        <div class="relative top-[90px] cursor-pointer animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
           <svg class="w-6 h-6 text-cyan-bright" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
             <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
           </svg>

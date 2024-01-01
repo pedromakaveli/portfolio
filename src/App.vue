98a965fe2ad9e355822d546df99e68884084c43d
pedromakaveli <pedrolacerda804@gmail.com>
2024-01-01 16:58:13 -0300

    Now the navbar is a vue component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <template>
   <div class="bg-slate-900 min-h-screen" id="app">
-    <!-- Inicia o o menu de navegação -->
-    <nav id="navtop" class="white flex justify-center">
-      <ul class="z-10 px-10 flex flex-wrap mt-10 items-center gap-2 font-mono text-md text-slate-50 fixed" :class="scrollPosition ? 'transition duration-500 ease-in-out bg-menu rounded-full backdrop-blur-md' : 'transparent'">
-        <li class="grid hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">
-          <a href="#navtop">// home</a>
-        </li>
 
-        <li class="hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">
-          <a href="#portfolio">// portfólio</a>
-        </li>
-
-        <li class="hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">// formação</li>
-        <li class="hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">// contato</li>
-      </ul>
-    </nav>
-
-    <!-- finaliza o menu de navegação --> 
+    <navbar></navbar>
 
     <introduction id="intro"></introduction>
 
@@ -28,13 +13,15 @@
 <script>
 import portfolioSection from './components/portfolioSection';
 import introSection from './components/introSection.vue';
-
+import navbarMenu from './components/navbarMenu.vue';
 export default {
 
   name: 'App',
   components: {
     'my-portfolio': portfolioSection,
-    'introduction': introSection
+    'introduction': introSection,
+    'navbar': navbarMenu
+    
   },
   data() {
     return {
---
+++ src/components/navbarMenu.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav id="navtop" class="white flex justify-center">
+        <ul class="z-10 px-10 flex flex-wrap mt-10 items-center gap-2 font-mono text-md text-slate-50 fixed"
+            :class="scrollPosition ? 'transition duration-500 ease-in-out bg-menu rounded-full backdrop-blur-md' : 'transparent'">
+            <li class="grid hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">
+                <a href="#navtop">// home</a>
+            </li>
+
+            <li class="hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">
+                <a href="#portfolio">// portfólio</a>
+            </li>
+
+            <li class="hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">// formação</li>
+            <li class="hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">// contato</li>
+        </ul>
+    </nav>
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      scrollPosition: null
+    };
+  },
+
+  methods: {
+    onScroll () {
+      this.scrollPosition = window.scrollY
+      console.log(this.scrollPosition)
+    }
+    ,
+  },
+
+  mounted() {
+    window.addEventListener('scroll', this.onScroll);
+  }
+}
+</script>

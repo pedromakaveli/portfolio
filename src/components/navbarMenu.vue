02d6e1984f5d8d45d5fd63e7828116395b9635f0
pedromakaveli <pedrolacerda804@gmail.com>
2024-01-02 19:11:00 -0300

    Navbar links optimized

--- src/components/navbarMenu.vue
+++ src/components/navbarMenu.vue
@@ -2,16 +2,10 @@
     <nav id="navtop" class="white flex md:justify-center justify-end text-white">
         <ul class="z-10 px-10 hidden md:flex flex-wrap mt-10 items-center gap-2 font-mono text-md text-slate-50 fixed"
             :class="scrollPosition ? 'transition duration-500 ease-in-out bg-menu rounded-full backdrop-blur-md' : 'transparent'">
-            <li class="grid hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">
-                <a href="#navtop">// home</a>
+            <li v-for="(link, index) in menuLinks" :key="index" class="grid hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">
+                <a :href="link.link">{{link.nome}}</a>
             </li>
 
-            <li class="hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">
-                <a href="#portfolio">// portfólio</a>
-            </li>
-
-            <li class="hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">// formação</li>
-            <li class="hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">// contato</li>
         </ul>
         <div class="md:hidden flex flex-col p-5">
             <div>
@@ -26,7 +20,13 @@
 export default {
     data() {
         return {
-            scrollPosition: null
+            scrollPosition: null,
+            menuLinks: [
+                {'nome': '// home', 'link': '#intro'},
+                {'nome': '// portfolio', 'link': '#portfolio'},
+                {'nome': '// formação', 'link': '/'},
+                {'nome': '// contato', 'link': '/'},
+            ]
         };
     },
 

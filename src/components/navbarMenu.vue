323fe4043c4da4b36d4f3f28cd96a3335835d7c7
pedromakaveli <pedrolacerda804@gmail.com>
2024-01-02 17:55:07 -0300

    Makes desktop menu invisible to mobile devices

--- src/components/navbarMenu.vue
+++ src/components/navbarMenu.vue
@@ -1,6 +1,6 @@
 <template>
     <nav id="navtop" class="white flex justify-center">
-        <ul class="z-10 px-10 flex flex-wrap mt-10 items-center gap-2 font-mono text-md text-slate-50 fixed"
+        <ul class="z-10 px-10 hide invisible sm:flex flex-wrap mt-10 items-center gap-2 font-mono text-md text-slate-50 fixed"
             :class="scrollPosition ? 'transition duration-500 ease-in-out bg-menu rounded-full backdrop-blur-md' : 'transparent'">
             <li class="grid hover:text-cyan-400 transition duration-150 cursor-pointer p-4 font-bold">
                 <a href="#navtop">// home</a>

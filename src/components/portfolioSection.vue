2578bdde1907f5f823fbb7495934d283a8b5980b
pedromakaveli <pedrolacerda804@gmail.com>
2024-01-02 18:02:13 -0300

    Adjust the responsiveness of the portfolio and title (provisional)

--- src/components/portfolioSection.vue
+++ src/components/portfolioSection.vue
@@ -2,12 +2,11 @@
     <!-- Inicia a div do portfolio -->
     <div class="bg-slate-900 pt-5 pb-5 px-20">
         <div class="container mx-auto flex flex-col">
-            <h1 id="portfolio"  class="cursor-pointer text-center font-montserrat text-sky-100 text-[2rem] font- hover:text-cyan-400"><span
-                    class="text-cyan-400 text-[2.2rem]">{</span> Meus projetos <span
-                    class="text-cyan-400 text-[2.2rem]">}</span>
+            <h1 id="portfolio"  class="cursor-pointer text-center font-montserrat text-sky-100 text-[1.5rem] md:text-[2rem] font- hover:text-cyan-400">
+                <span class="text-cyan-400 text-[2.2rem]">{</span> Meus projetos <span class="text-cyan-400 text-[2.2rem]">}</span>
             </h1>
 
-            <div class="mt-10 grid grid-cols-2 md:grid-cols-4 gap-4">
+            <div class="mt-10 grid grid-cols-2 max-[600px]:grid-cols-1 md:grid-cols-4 gap-4">
                     <div v-for="(image, index) in images" :key="index" class="grid gap-4">
                         <img :src="image" class="rounded-lg h-auto max-w-full cursor-pointer" alt="">
                     </div>

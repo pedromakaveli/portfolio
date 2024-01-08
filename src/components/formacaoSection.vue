414b37c35f65c1acc161238fa8a548990039534f
pedromakaveli <pedrolacerda804@gmail.com>
2024-01-08 11:47:40 -0300

    Add type of course on formaction cards

--- src/components/formacaoSection.vue
+++ src/components/formacaoSection.vue
@@ -6,14 +6,19 @@
                 <span class="text-cyan-400 text-[2.2rem]">{</span>🎓 Formação <span class="text-cyan-400 text-[2.2rem]">}</span></h1>
 
                 <div class="flex justify-center content-center items-center min-h-[70vh] gap-10">
-                    <div v-for="(formacao, index) in formacao" :key="index" class="flex flex-col gap-2 h-[300px] w-[300px] px-10 py-10 course bg-black rounded-lg">
-                        <h1 :class="{
-                                'detail-blue': formacao.instituicao === 'Estácio' || formacao.instituicao === 'CEFET-MG',
-                                'detail-purple': formacao.instituicao === 'Origamid'
-                            }" class="flex-grow text-white text-[1.8rem] font-montserrat uppercase font-bold">
-                            {{formacao.instituicao}}
-                        </h1>
-                        <h2 class="text-center flex-grow text-zinc-300 text-[0.90rem] font-montserrat uppercase">{{formacao.curso}}</h2>
+                    <div v-for="(formacao, index) in formacao" :key="index" class="flex flex-col gap-2 h-[400px] w-[400px] px-10 py-10 course bg-black rounded-lg hover:scale-[105%] ease-in-out duration-150">
+                        
+                        <div class="flex-grow cursor-pointer">
+                            <h1 :class="{
+                                    'detail-blue estacio-title': formacao.instituicao === 'Estácio' || formacao.instituicao === 'CEFET-MG',
+                                    'detail-purple origamid-title': formacao.instituicao === 'Origamid'
+                                }" class="flex-grow text-white text-[1.8rem] font-montserrat uppercase font-bold">
+                                {{formacao.instituicao}}
+                
+                            </h1>
+
+                            <span class="text-white font-montserrat text-[1rem]">{{formacao.tipo}} em {{formacao.curso}}</span>
+                        </div>
                     </div>
                     
                 </div>
@@ -29,9 +34,9 @@ export default {
     data() {
         return {
             formacao: [
-                {'curso': 'Análise e Desenvolvimento de Sistemas', 'instituicao': 'Estácio'},
-                {'curso': 'Front-end e Design UI', 'instituicao': 'Origamid'},
-                {'curso': 'Desenvolvimento Web', 'instituicao': 'CEFET-MG'},
+                {'curso': 'Análise e Desenvolvimento de Sistemas', 'instituicao': 'Estácio', 'tipo': 'Tecnólogo'},
+                {'curso': 'Front-end e Design UI', 'instituicao': 'Origamid', 'tipo': 'Curso livre'},
+                {'curso': 'Desenvolvimento Web', 'instituicao': 'CEFET-MG', 'tipo': 'Curso Livre'},
             ],
 
             schoolSelected: []

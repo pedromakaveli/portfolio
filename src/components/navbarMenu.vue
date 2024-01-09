f1b3197b9fc91d4dfa68038993402a7425feaa1c
pedromakaveli <pedrolacerda804@gmail.com>
2024-01-09 20:37:32 -0300

    Add mobile icon on nav and disable bg color on mobile screens

--- src/components/navbarMenu.vue
+++ src/components/navbarMenu.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="fixed top-0 left-0 w-[100vw] px-5 py-[25px] z-[5]" :class="scrollPosition && activeMobile != true ? 'transition duration-500 ease-in-out bg-menu backdrop-blur-sm' : 'mt-0'">
+    <header class="bg-transparent backdrop-blur-none fixed md:fixed top-0 left-0 w-[100vw] px-5 py-[25px] z-[5]" :class="scrollPosition ? 'transition duration-500 ease-in-out bg-menu backdrop-blur-sm' : 'mt-0'">
             
         <nav class="flex justify-end md:flex justify-center">
             <ul class="hidden md:flex items-center content-center flex-wrap text-cyan-bright font-roboto-mono text-[1rem] gap-10">
@@ -7,6 +7,10 @@
                     <a :href="link.link">{{ link.nome }}</a>
                 </li>
             </ul>
+
+            <div class="md:hidden">
+                <font-awesome-icon class="p-5 absolute top-0 right-0 cursor-pointer" @click="activeMobile = ! activeMobile" :icon="activeMobile ? ['fas', 'fa-circle-xmark'] : ['fas', 'bars']" size="2xl" style="color: #ffffff;" />
+            </div>
 
         </nav>
     </header>

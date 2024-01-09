a4fc51e1134124716769341df3be7593a18a69fd
pedromakaveli <pedrolacerda804@gmail.com>
2024-01-09 20:31:56 -0300

    Responsive adjusts on nav

--- src/components/navbarMenu.vue
+++ src/components/navbarMenu.vue
@@ -1,8 +1,8 @@
 <template>
     <header class="fixed top-0 left-0 w-[100vw] px-5 py-[25px] z-[5]" :class="scrollPosition && activeMobile != true ? 'transition duration-500 ease-in-out bg-menu backdrop-blur-sm' : 'mt-0'">
             
-        <nav class="flex justify-center">
-            <ul class="flex items-center content-center flex-wrap text-cyan-bright font-roboto-mono text-[1rem] gap-10">
+        <nav class="flex justify-end md:flex justify-center">
+            <ul class="hidden md:flex items-center content-center flex-wrap text-cyan-bright font-roboto-mono text-[1rem] gap-10">
                 <li v-for="link, index in menuLinks" :key="index" class="px-[10px] py-[px]">
                     <a :href="link.link">{{ link.nome }}</a>
                 </li>
